5fedbb86bf2c063a4fa40b48e9253b5a3153327e
Touraj Ostovari <tornet656@gmail.com>
2019-10-25 12:26:17 +0330

    Add project files.

---
+++ winform_first_practise/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace winform_first_practise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Source_Y = new System.Windows.Forms.TextBox();
+            this.Source_X = new System.Windows.Forms.TextBox();
+            this.Dest_Y = new System.Windows.Forms.TextBox();
+            this.Dest_X = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(663, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Y:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(663, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(663, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Y:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(663, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "X:";
+            // 
+            // Source_Y
+            // 
+            this.Source_Y.Location = new System.Drawing.Point(666, 46);
+            this.Source_Y.Name = "Source_Y";
+            this.Source_Y.Size = new System.Drawing.Size(122, 20);
+            this.Source_Y.TabIndex = 4;
+            // 
+            // Source_X
+            // 
+            this.Source_X.Location = new System.Drawing.Point(666, 92);
+            this.Source_X.Name = "Source_X";
+            this.Source_X.Size = new System.Drawing.Size(122, 20);
+            this.Source_X.TabIndex = 5;
+            // 
+            // Dest_Y
+            // 
+            this.Dest_Y.Location = new System.Drawing.Point(666, 194);
+            this.Dest_Y.Name = "Dest_Y";
+            this.Dest_Y.Size = new System.Drawing.Size(122, 20);
+            this.Dest_Y.TabIndex = 6;
+            // 
+            // Dest_X
+            // 
+            this.Dest_X.Location = new System.Drawing.Point(666, 245);
+            this.Dest_X.Name = "Dest_X";
+            this.Dest_X.Size = new System.Drawing.Size(122, 20);
+            this.Dest_X.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(590, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Source:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(590, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Destination:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(666, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Draw it...";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Dest_X);
+            this.Controls.Add(this.Dest_Y);
+            this.Controls.Add(this.Source_X);
+            this.Controls.Add(this.Source_Y);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Source_Y;
+        private System.Windows.Forms.TextBox Source_X;
+        private System.Windows.Forms.TextBox Dest_Y;
+        private System.Windows.Forms.TextBox Dest_X;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ winform_first_practise/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_first_practise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //this.Paint += 
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            Pen pen = new Pen(System.Drawing.Color.Black, 5.0f);
+
+            int y2 = 20; // x2
+            float y1 = 30.0f; // x1
+            //==============
+            int x2 = 180; // y2
+            float x1 = 50; // y1
+            //==============
+            float rep_x1 = x1;
+            float rep_y1 = y1;
+            System.Collections.Generic.List<float> collect = new List<float>();
+            
+
+            System.Collections.Generic.Dictionary<int, int> pairs = new Dictionary<int, int>();
+
+            float m =float.Parse(((float.Parse((y2 - y1).ToString()) / float.Parse((x2 - x1).ToString())).ToString("f3")));
+            if(m>1)
+            {
+                while ((x1 <= x2 || x1 != x2) && (y1 != y2))
+                {
+                    collect.Add(x1);
+                    x1 += m;
+                    y1 += 1.0f;
+                }
+                for (float i = rep_y1; i <= y2;)
+                {
+                    try
+                    {
+                        graphics.DrawLine(pen, i++, float.Parse((Math.Round(collect[(int)i])).ToString()), i, float.Parse((Math.Round(collect[(int)i])).ToString()));
+                    }
+                    catch (Exception)
+                    { }
+                }
+            }
+            else if(m<1)
+            {
+                while ((x1!=x2) && (y1<=y2 || y1!=y2))
+                {
+                    collect.Add(y1);
+                    y1 += m;
+                    x1+=1;
+                }
+                for (float i = rep_x1; i <= x2;)
+                {
+                    try
+                    {
+                        graphics.DrawLine(pen, i++, float.Parse((Math.Round(collect[(int)i])).ToString()), i, float.Parse((Math.Round(collect[(int)i])).ToString()));
+                    }
+                    catch (Exception)
+                    {}
+                }
+            }
+            else if(m == 1)
+            {
+                graphics.DrawLine(pen, x1, y1, x2, y2);
+            }
+            graphics.Dispose();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
